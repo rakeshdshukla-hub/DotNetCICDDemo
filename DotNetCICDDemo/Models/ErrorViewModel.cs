2c2e96213414bde7bb3de177ee2052fe6cde7945
Rakesh.shukla <rakesh.d.shukla@live.com>
2026-02-21 11:55:07 +0530

    Add project files.

---
+++ DotNetCICDDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCICDDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
